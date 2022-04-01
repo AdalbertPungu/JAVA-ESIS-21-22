c91cee7ac6567dccc7ea277717c4e86869d95f1d
Adalbert Pungu <18pw419@esisalama.org>
2022-04-01 19:27:26 +0200

    Update ConvertirGallons.java

--- convertirGallons/src/ConvertirGallons.java
+++ convertirGallons/src/ConvertirGallons.java
@@ -2,6 +2,15 @@ import java.util.Scanner;
 
 public class ConvertirGallons {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        // double gallon = 5;
+        // double liter = gallon*3.785;
+        // System.out.println(gallon + " gallons est equivalent a " + liter + " liters");
+
+        System.out.println("combien de gallons souhaitez-vous convertir ?");  
+		double gallon = sc.nextDouble();    
+		double liter = gallon * 3.8;
+		System.out.println(gallon + " gallons equivaut a: " + liter + " liters");
     }
 }
+
